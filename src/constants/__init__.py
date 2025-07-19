6b1d9aa2a93470b48d04ba402bbcacbee2eefdb6
vineet416 <vineetpatel468@gmail.com>
2025-07-19 20:54:31 +0530

    setup constants

---
+++ src/constants/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+MONGO_DATABASE_NAME = "credit_card_transactions_database"
+MONGO_COLLECTION_NAME = "credit_card_transactions"
+MONGO_DB_URL = ""
+
+
+TARGET_COLUMN = "Class"
+
+ISOLATION_FOREST_MODEL_NAME = "isolation_forest_model.pkl"
+MODEL_FILE_NAME = "model.pkl"
+
+ARTIFACT_FOLDER = "artifacts"
